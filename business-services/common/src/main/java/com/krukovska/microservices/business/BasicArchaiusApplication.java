4f4533336f6fda9b7ed5d84152417ac08b838b95
YanaKrukovska <jana.krua@gmail.com>
2021-10-11 00:55:00 +0300

    Add common module with archaius

---
+++ business-services/common/src/main/java/com/krukovska/microservices/business/BasicArchaiusApplication.java
@@ -0,0 +1,13 @@
+package com.krukovska.microservices.business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicArchaiusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicArchaiusApplication.class, args);
+    }
+
+}
